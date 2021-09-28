65678dab0454efa80b0536b761e880688959fdd8
heraldOfMechanus <jccallahan11@gmail.com>
2021-09-28 13:47:58 -0500

    Initialized test skeleton

---
+++ src/main/java/com/revature/delete_user_favorites/DeleteUserFavoritesHandler.java
@@ -0,0 +1,18 @@
+package com.revature.delete_user_favorites;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+public class DeleteUserFavoritesHandler implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
+
+    private static final Gson mapper = new GsonBuilder().setPrettyPrinting().create();
+
+    @Override
+    public APIGatewayProxyResponseEvent handleRequest(APIGatewayProxyRequestEvent apiGatewayProxyRequestEvent, Context context) {
+        return null;
+    }
+}
---
+++ src/test/java/com/revature/delete_user_favorites/DeleteUserFavoritesHandlerTest.java
@@ -0,0 +1,22 @@
+package com.revature.delete_user_favorites;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DeleteUserFavoritesHandlerTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void handleRequest() {
+    }
+}
